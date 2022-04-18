257dddcf5e893ec6ec86883488a5a2287ab68bd7
jeannoelvega <jeannoel.vega@gmail.com>
2022-04-18 15:17:43 +0200

    insertion dans le repositorie

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# Test-P6
+# Projet 6 - OpenClassrooms
+
+------
+
+## Script installant divers services sur divers postes en réseau
+
+------
+
+### Pour faire fonctionner ce script, il faut plusieurs prérequis:
+* Les serveurs sont tous sous débian ou une distribution basé sur débian.
+* Un des serveurs sera le noeud principal où sera installé obligatoirement Ansible.
+* Sur le noeud principal, il faut obligatoirement Python version 3.10 ou ultérieur.
+* Les autres serveurs auront un acces SSH en root avec le même mot de passe pour tous.
+* Les adresses IP des noeuds secondaires seront dans le fichier fichier.txt.
+* Les templates d'Ansible seront dans le répertoire playbooks dans le noeud principal.
+
+# Le script ansible.py 
+
+Il permet d'installer automatiquement Ansible sur le noeud principal.
+
+# Le script projet06.py
+
+Ce script permet de selectionner le premier poste libre et d'installer le service choisi.
+
+# Le fichier config.ini
+
+Ce fichier permet de modifier à sa convenance la liste des services.
+Le menu affichera cette liste.
+
+le fichier se présente ainsi:
+1 http /chemin/fichier/yaml
+2 dns /chemin/fichier/yaml
+
+# Le fichier fichier.txt
+
+Ce fichier liste les adresses IP des noeuds secondaires où seront installé les services.
+
+Les adresses IP libres sont indiquées ainsi:
+xxx.xxx.xxx.xxx  libre
+
+Les adresses IP utilisées sont indiquées comme suit:
+xxx.xxx.xxx.xxx
+
---
+++ projet06.py
@@ -0,0 +1,153 @@
+# variables globales
+
+global adrIp, adressIp, plbook
+
+# MODULES 
+
+# Permet de lancer des commandes avec le system
+import subprocess
+
+# Permet d'utiliser des commandes courantes de tous les OS
+import os
+
+# Permet le 'logging'
+import logging
+
+# Paramètrage du logging
+logging.basicConfig(filename='projet06.log', encoding='utf-8', level=logging.INFO)
+logging.info('démarrage de retest.py')
+
+# FONCTIONS
+
+# Fonction de nettoyage des fichiers temporaires
+def removeTemp():
+    if os.path.exists('fichtemp.txt'):
+        os.remove('fichtemp.txt')
+    else:
+        print("fichtemp.txt n'existe pas \n")
+
+    if os.path.exists('fichyaml.yml'):
+        os.remove('fichyaml.yml')
+logging.info('Nettoyage des fichiers temporaires effectuée')
+
+
+# Fonction qui récupère l'adresse IP d'une machine libre
+def choixserver():
+    chaine = "libre"  # text à rechercher
+    fichtemp = open("fichtemp.txt", "r")  # ouvre le fichier txt temporaire
+    ipfind = False      # pour l'instant, on n'a pas trouvé d'IP
+    for ligne in fichtemp:  #  parcoure fichtemp.txt
+        if chaine in ligne: #  si on trouve le mot "libre"...
+            ipfind = True   #  ...c'est qu'on a une adresse IP
+            global adressIp
+            adressIp = ligne.split()    # découpe la ligne en liste de str
+            adressIp.remove( "libre")   # enleve le mot "libre" de la liste 
+            global adrIp
+            adrIp = adressIp
+            break
+    fichtemp.close()
+
+    if ipfind == False:     #  si on n'a pas trouvé d'adresse IP libre
+        print("il n'y a plus de machines libres")
+        logging.info("il n'y a plus de machine libre")
+        exit()
+
+    logging.info('IP libre trouvé')
+    return adrIp
+
+# Fonction qui modifie la ligne ou l'IP était libre en IP non libre
+def modifIp():
+    global adrIp                 
+    adrIp = ' '.join(adrIp)                 # met en format str
+    chaine = adrIp  
+    fichtemp = open("fichtemp.txt", "r")    # ouvre le fichier temporaire en lecture seule
+    fichier = open("fichier.txt", "w")      # ouvre fichier.txt en écriture
+    for ligne in fichtemp:                  # lis chaque ligne de fichtemp dans une boucle for
+        if chaine in ligne:                 # si l'adresse IP est dans la ligne...
+            adressIp = ligne.split()        # découpe la ligne en liste de str
+            adressIp.remove( "libre")       # enleve le mot "libre" de la liste
+            adressIp = ' '.join(adressIp)   # transforme la liste en str
+            adressIp = adressIp + "\n"      # ajoute un saut de ligne
+            ligne = adressIp                # met la valeur dans la variable ligne
+        fichier.write(ligne)                # écrit la ligne dans le fichier texte
+    fichtemp.close()                        # ferme le fichier temporaire
+    fichier.close()                         # ferme fichier.txt
+    logging.info('fichier.txt modifié')
+
+# Fonction qui crée le fichier yaml temporaire pour lancer la commande Ansible
+def createFichyaml():
+    global adrIp
+    fichier = open(plbook, "r")             # ouvre le template yaml en lecture
+    lignes = fichier.readlines()            # enregistre chaque ligne dans une liste
+    fichier.close()                         # ferme le template yaml
+    lignes[1] = "- hosts: " + adrIp + "\n"  # modifie la seconde ligne avec l'adresse IP trouvé
+    fichtemp = open("fichyaml.yml", "w")    # ouvre le fichier yaml temporaire
+    fichtemp.writelines(lignes)             # recopie les lignes
+    fichtemp.close()                        # ferme le fichier yaml temporaire
+    logging.info('Création du fichier yaml terminée')
+
+# Fonction qui crée un fichier temporaire pour la recherche d'une machine libre 
+# et pour modifier cette machine en occupé.
+def copyfich():
+# copie le fichier des adresses IP à l'identique dans un fichier temporaire
+    fichier = open("fichier.txt", "r")
+    fichtemp = open("fichtemp.txt", "w")
+    fichtemp.write(fichier.read() )
+    fichier.close()
+    fichtemp.close()
+    logging.info('Création du fichier temporaire txt terminée')
+
+# Fonction qui installe les playbooks
+def installplaybook():
+    subprocess.call(["ansible-playbook", "/root/fichyaml.yml"])
+    print("playbook lancé ")
+    logging.info('Playbook lancé')
+
+# Fonction principal qui lance les autres fonctions dans l'ordre
+def principal():
+
+    copyfich()
+    choixserver()
+    modifIp()
+    createFichyaml()
+    installplaybook()
+    removeTemp()
+
+########################## MENU ################################################################
+
+# Création des dictionnaires vides
+listService = {}
+listPlaybook = {}
+
+# Remplissage des dictionnaires avec notre menu
+fileini = open("config.ini", "r")       #fichier de configuration de notre menu
+for line in fileini :
+    key = int(line.split()[0])
+    name = line.split()[1]
+    path = line.split()[2]
+    listService[key] = name
+    listPlaybook[key] = path
+fileini.close()
+
+# récupère le nombre d'items dans notre menu
+tailleDic = len(listService)
+
+# affiche notre menu
+for key, val in listService.items():
+    print(key, " - ", val)
+
+print(0, " - sortie")   # Rajoute la sortie du programme
+
+# Boucle de choix avec gestion des erreurs
+inputVal = input("Faites votre choix ")
+inputVal = int(inputVal)
+if inputVal == 0:
+    exit(0)
+elif inputVal > tailleDic:
+    print("Ce choix n'est pas dans la liste")
+else:
+#    print(listPlaybook[inputVal])
+    plbook = listPlaybook[inputVal]
+#    plbook = "squid.yml"       # Pour tests
+    principal()
+
